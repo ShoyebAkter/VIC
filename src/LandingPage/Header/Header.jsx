4d4e45ad2af7c18aba936e1d0e5556aa5719b837
Shoyeb <heroreal5385@gmail.com>
2024-08-19 11:24:29 +0600

    navbar padding rmv

--- src/LandingPage/Header/Header.jsx
+++ src/LandingPage/Header/Header.jsx
@@ -1,28 +1,32 @@
 import React from "react";
 import "./Header.css";
-const Header = ({setLanguage}) => {
+const Header = ({ setLanguage }) => {
   const handleLanguageChange = (event) => {
     setLanguage(event.target.value);
   };
   return (
     <div className="header-container">
       <nav className="navbar">
-      <div><img className="headerLogo" src="/logo2.png" alt=""/></div>
+        <div>
+          <img className="headerLogo" src="/logo2.png" alt="" />
+        </div>
         <div className="nav-item highlight">About VIC</div>
         <div className="nav-item">Where We Work</div>
         <div className="nav-item highlight">Services</div>
         <div className="nav-item highlight">Pricing</div>
         <div className="nav-item button">Book A Consultation</div>
       </nav>
-      <div className="centered-title"><img className="vic" src="/logo1.png" alt=""/></div>
+      <div className="centered-title">
+        <img className="vic" src="/logo1.png" alt="" />
+      </div>
       <div className="bookButtonArea">
-      <div className="bookButton">Book Now</div>
-      <div class="arrow-container">
-        <div class="arrow-down"></div>
-        <div class="arrow-down"></div>
+        <div className="bookButton">Book Now</div>
+        <div class="arrow-container">
+          <div class="arrow-down"></div>
+          <div class="arrow-down"></div>
+        </div>
       </div>
-      </div>
-      
+
       <div className="language-dropdown">
         Language:
         <select onChange={handleLanguageChange}>
