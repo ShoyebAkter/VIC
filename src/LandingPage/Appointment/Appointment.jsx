5211d0514a23461a0382f973b2d7d8b68b1fc076
Shoyeb <heroreal5385@gmail.com>
2024-10-10 19:44:26 +0600

    bg change

--- src/LandingPage/Appointment/Appointment.jsx
+++ src/LandingPage/Appointment/Appointment.jsx
@@ -22,7 +22,7 @@ const Appointment = () => {
       }
     }
   return (
-    <div style={{backgroundColor:"#171717" ,color:"white"   }}>
+    <div style={{backgroundColor:"#171717" ,color:"white"}}>
       <div className="flex justify-between mx-10 pt-4">
         <div onClick={handleBack} className="font-bold cursor-pointer flex items-center gap-1"><IoIosArrowBack /> Back</div>
         <div onClick={()=>navigate('/')} className="font-bold cursor-pointer"> X </div>
